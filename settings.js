2b2d47115c349207bc31e7e9a846d11d996ca952
Symplylonely2 <128530638+Symplylonely2@users.noreply.github.com>
2023-12-31 12:23:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 
 //aumto presence update
 global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.autoblockmorroco = false //auto block 212 (true to on, false to off)
 global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
 global.antispam = false //auto kick spammer (true to on, false to off)
@@ -21,28 +21,28 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.owner = ['2348184186086'] //ur owner number
+global.ownernomer = "2348184186086" //ur owner number2
+global.ownername = "♈️𝐓𝐇𝐄 𝐔𝐍𝐊𝐍𝐎𝐖𝐍 𝐆𝐔𝐘♈️" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "nigeria, lagos, abuja" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.botname = "♈️𝐓𝐇𝐄 𝐔𝐍𝐊𝐍𝐎𝐖𝐍 𝐁𝐎𝐓♈️"
+global.ownernumber = '233246274343'
+global.ownername = '♈️𝐓𝐇𝐄 𝐔𝐍𝐊𝐍𝐎𝐖𝐍 𝐆𝐔𝐘♈️'
+global.ownerNumber = ["2348184186086@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
-global.themeemoji = '🪀'
+global.wagc = "https://chat.whatsapp.com/Go4drrshNlf5v5SAIyQ535"
+global.themeemoji = '♈️'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
-global.prefa = ['','!','.','#','&']
+global.packname = "♈️—•"
+global.author = "𝐓𝐇𝐄 𝐔𝐍𝐊𝐍𝐎𝐖𝐍 𝐁𝐎𝐓♈️\n\n"
+global.creator = "2348184186086@s.whatsapp.net"
+global.prefa = ['','!','.','#','♈️']
 global.sessionName = 'session'
 global.hituet = 0
 
